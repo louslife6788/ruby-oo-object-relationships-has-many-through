7f13874f3dd702916f039c26b2da2aee0525cc27
luis leon <leonluis380@yahoo.com>
2021-07-05 11:15:49 -0400

    commit tree 0867c91d254ecf9cac38eaefd8380fc383a72141

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
+  @@all = []
+    
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+
+    @name= name 
+    @age = age 
+    @@all << self 
+  end 
+
+
+  def self.all 
+    @@all
+  end 
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals 
+    Meal.all.select {|food| food.customer == self }
+  end
+
+  def waiters
+    meals.map {|plate| plate.waiter}
+  end 
+
   
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
+require "pry"
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+
+    @@all = []
+  
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer , total, tip)
+    end 
+
+    def meals
+        Meal.all.select { |plate| plate.waiter == self}
+    end 
+
+    def best_tipper 
+        tip = 0 
+        cust = ""
+        meals.each do |meal|
+           if meal.tip > tip
+                tip = meal.tip
+                cust = meal.customer
+           end
+        end
+        cust
+            
+    end 
+
+ 
 end
